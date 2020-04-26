255b5ada1fa92cf13d744be2d38d8145790cf40b
mmariotti <mmariotti@mariotti-ltm.internal.salesforce.com>
2020-04-26 17:05:30 -0300

    Ejercicios encoding de Church: and, or, not.

--- ejercicios1.clj
+++ ejercicios1.clj
@@ -1,3 +1,35 @@
-   (defn funtrue2 [x] (defn f3 [y] x))
-   (defn funfalse2 [x] (defn f4 [y] y))
-   (defn toBoolean [f] ((f true) false))
+;; funTrue
+;; receives two values A and B
+;; returns first value
+(defn funTrue [x] (defn f3 [y] x))
+
+;; funFalse
+;; receives two values A and B
+;; returns second value
+(defn funFalse [x] (defn f4 [y] y))
+
+;; toBoolean 
+;; receives funTrue or funFalse and
+;; returns true if funTrue was received
+;; returns false if funFalse was received
+(defn toBoolean [f] ((f true) false))
+
+;; funNot
+;; receives funTrue or funFalse
+;; if input is funFalse returns true
+;; if input is funTrue returns true
+(defn funNot [x] ((x false) true))
+
+;; funAnd
+;; receives two inputs which can take values: funTrue or funFalse
+;; if input is funTrue,funTrue then true is returned
+;; otherwise returns false
+(defn funAnd [x] (defn f1 [y] ((y ((x true) false)) false)))
+
+;; funOr
+;; receives two inputs which can take values: funTrue or funFalse
+;; if input is funFalse,funFalse then false is returned
+;; otherwise returns true
+(defn funOr [x] (defn f1 [y] ( (y true) ((x true) false) ) ))
+
+;; funXOR
